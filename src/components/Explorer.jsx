0edcad35f9390b6fa4c7013fe9647d9695f066e6
Niall Antony <88824327+niallantony@users.noreply.github.com>
2024-12-30 17:09:28 +0900

    Page for adding students quickly

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const ExplorerLayout = styled.div`
+export const Explorer = styled.div`
  border: solid 1px ${props => props.theme.accent};
 border-radius: 4px;
 height: 100%;
@@ -9,36 +9,145 @@ display: flex;
 
 const StyledSelect = styled.div`
  border-right: solid 1px ${props => props.theme.accent};
-flex: 1;
+flex: 1 ;
+display: flex;
+flex-direction: column;
+padding: 1rem;
 `;
 
 const StyledView = styled.div`
-
-flex: 2;
+flex: 3;
 `
 
-export function Explorer({ children }) {
-
-  return (
-    <ExplorerLayout>
-      {children}
-    </ExplorerLayout>
-  )
-
+const SelectChoice = styled.li`
+padding: 0 1rem;
+list-style:none;
+&:before{
+  content:"-";
+  margin-right: 1rem;
 }
+text-wrap:nowrap;
+overflow: hidden;
+white-space: nowrap;
+text-overflow: ellipsis;
+text-decoration: none;
+border-bottom: ${props => props.$active ? `solid 1px ${props.theme.accent}` : 'none'};
+background-color: ${props => props.$active ? `${props.theme.hover}` : 'none'};
+margin-bottom: 0.5rem;
+&:hover {
+  color: ${props => props.theme.accent};
+}
+`
+const Label = styled.label`
+height: 2rem;
+line-height: 2rem;
+margin-right: 1rem;
+`
+const Input = styled.input`
+    background-color: ${props => props.theme.light};
+border: 0;
+    border-bottom: solid 1px ${props => props.theme.accent};
+  
+    padding: 0.5rem;
+    color: black;
+    height: fit-content;
+    &:disabled {
+      border: solid 1px rgba(118, 118, 118, 0.3);
+      color: rgba(118, 118, 118, 0.3);
+    }
+`
+const StyledInput = styled.div`
+  display: flex;
+
+`
+export const StyledAddRow = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const RowButton = styled.button`
+  border: 0;
+  background-color: transparent; 
+color: ${props => props.theme.accent};
+width: 2rem;
+height: 2rem;
+padding: 0;
+&:hover {
+  transform: scale(1.5);
+}
+&:active {
+  transform: translateY(5px);
+}
+
+`
+export const RowInfo = styled.div`
+widht: 100%;
+display: grid;
+grid-template-columns: 1fr 6fr;
+border-bottom: solid 1px ${props => props.theme.accent};
+margin: 0 1rem;
+padding: 0.5rem 0;
+&:hover {
+  background-color: ${props => props.theme.hover}
+}
+> p {
+  margin: 0 1rem;
+}
+`
 
 export function ExplorerSelect({ children }) {
 
   return (
     <StyledSelect>
+      {children}
     </StyledSelect>
   )
+}
+
+export function ExplorerSelectChoice({ active = false, children, onClick, id }) {
+  const click = () => {
+    onClick(id)
+  }
+  return (
+    <SelectChoice onClick={click} $active={active}>
+      {children}
+    </SelectChoice>
+  )
+
 }
 
 export function ExplorerView({ children }) {
 
   return (
     <StyledView>
+      {children}
     </StyledView>
+  )
+}
+
+export function ViewInput({ id, value, onChange, name, disabled = false }) {
+  return (
+    <StyledInput className='text-input'>
+      <Label htmlFor={id}> {name}
+      </Label>
+      <Input
+        disabled={disabled}
+        id={id}
+        name={id}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </StyledInput>
+  )
+}
+
+export function AddRow({ children, onCancel, onSubmit }) {
+  return (
+    <>
+      <RowButton onClick={onCancel}>&times;</RowButton>
+      {children}
+      <RowButton type="submit" onClick={onSubmit}>+</RowButton>
+    </>
   )
 }
--- src/routes/Students.jsx
+++ src/routes/Students.jsx
@@ -1,12 +1,145 @@
-import { Explorer, ExplorerSelect, ExplorerView } from "../components/Explorer";
+const backendUrl = import.meta.env.VITE_BACKEND_URL
+import { useEffect, useState } from "react";
+import { AddRow, Explorer, ExplorerSelect, ExplorerSelectChoice, ExplorerView, RowButton, RowInfo, StyledAddRow, ViewInput } from "../components/Explorer";
 
 export function StudentExplorer() {
+  const [lessons, setLessons] = useState([])
+  const [selected, setSelected] = useState(0)
+  const [showAdd, setShowAdd] = useState(false)
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    fetchLessons()
+  }, [])
+
+  useEffect(() => {
+    if (lessons.length > 0) {
+      setSelected(lessons[0].lesson_id)
+    }
+
+  }, [lessons])
+
+  useEffect(() => {
+    fetchStudents()
+  }, [selected])
+
+  const fetchLessons = () => {
+    fetch(`${backendUrl}/lessons`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then(res => res.json())
+      .then(res => {
+        setLessons(res.lessons)
+      })
+
+  }
+
+  const fetchStudents = () => {
+    fetch(`${backendUrl}/students/${selected}`, {
+      method: "GET",
+      credentials: "include",
+    })
+      .then(res => res.json())
+      .then(res => {
+        setStudents(res.students)
+      })
+  }
+
+  const handleSubmit = () => {
+    fetchStudents()
+  }
+
+  const handleChange = (id) => {
+    setShowAdd(false)
+    setSelected(id)
+  }
+
+  const closeAdd = () => {
+    setShowAdd(false)
+  }
+  const displayError = (e) => {
+    console.log(e)
+  }
+
   return (
     <Explorer>
       <ExplorerSelect>
+        {lessons.map(lesson => {
+          return (
+            <ExplorerSelectChoice
+              active={lesson.lesson_id === selected}
+              id={lesson.lesson_id}
+              onClick={handleChange}
+              key={lesson.lesson_id}
+            >
+              {lesson.name}
+            </ExplorerSelectChoice>
+          )
+        })}
       </ExplorerSelect>
       <ExplorerView>
+        {students.map(student => {
+          return (
+            <RowInfo key={student.student_id}>
+              <p>{student.student_id}</p>
+              <p>{student.name}</p>
+            </RowInfo>)
+        })}
+        <StyledAddRow>
+          {showAdd ?
+            (<AddStudent onSubmit={handleSubmit} onClose={closeAdd} onError={displayError} lesson_id={selected} />) :
+            (<RowButton onClick={() => setShowAdd(true)}>+</RowButton>)
+          }
+        </StyledAddRow>
       </ExplorerView>
     </Explorer>
+  )
+}
+
+function AddStudent({ onError, onClose, onSubmit, lesson_id }) {
+  const [name, setName] = useState("")
+  const [studentId, setStudentId] = useState("")
+
+  const handleSubmit = () => {
+    fetch(`${backendUrl}/students/new`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({
+        name: name,
+        student_id: studentId,
+        lesson_id: lesson_id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (res.errors) {
+          onError(res.errors)
+        } else if (res.message === "Successful") {
+          onSubmit()
+          setName("")
+          setStudentId("")
+        }
+      })
+  }
+
+  return (
+    <AddRow onCancel={onClose} onSubmit={handleSubmit}>
+      <ViewInput
+        id="name"
+        name="Name"
+        value={name}
+        onChange={setName}
+      />
+      <ViewInput
+        id="student_id"
+        name="ID"
+        value={studentId}
+        onChange={setStudentId}
+      />
+    </AddRow>
   )
 }
